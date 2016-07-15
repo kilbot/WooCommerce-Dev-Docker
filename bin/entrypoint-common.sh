ab9fc811bb2862986acdaf95b5d1d319b0781e5c
kilbot <paul@kilbot.com.au>
2016-07-15 23:28:52 +0800

    abstract php entrypoint

---
+++ bin/entrypoint-common.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -e
+
+WP_VERSION=${WP_VERSION-latest}
+WC_VERSION=${WC_VERSION-latest}
+
+# install wordpress
+if ! [ -e index.php -a -e wp-includes/version.php ]; then
+  wp core download --allow-root --version=$WP_VERSION
+fi
+
+# wp-config
+if [ ! -e wp-config.php ]; then
+  wp core config --allow-root --skip-check --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --extra-php <<PHP
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'SCRIPT_DEBUG', true );
+define( 'WP_HOME', 'http://php'. PHP_MAJOR_VERSION . PHP_MINOR_VERSION .'.local' );
+define( 'WP_SITEURL', 'http://php'. PHP_MAJOR_VERSION . PHP_MINOR_VERSION .'.local' );
+PHP
+fi
+
+# set up db & plugins
+if ! $(wp core is-installed --allow-root); then
+  wp core install --allow-root --url=localhost --title=Localhost --admin_user=admin --admin_password=password --admin_email=support@wcpos.com
+  wp plugin install wordpress-importer --allow-root --activate
+  if [[ $WC_VERSION =~ [0-9]+\.[0-9]+(\.[0-9]+)? ]]; then
+    wp plugin install woocommerce --allow-root --version=$WC_VERSION --activate
+  else
+    wp plugin install woocommerce --allow-root --activate
+  fi
+  wp plugin activate woocommerce-pos --allow-root
+  wp import $(wp plugin path --allow-root)/woocommerce/dummy-data/dummy-data.xml --allow-root --skip=attachment --authors=create
+  wp option update woocommerce_api_enabled yes --allow-root
+  wp option update woocommerce_calc_taxes yes --allow-root
+  wp rewrite structure '/%year%/%monthnum%/%postname%' --allow-root
+  wp plugin deactivate wordpress-importer --allow-root
+#  wp rewrite flush --hard --allow-root
+fi
+
+# install unit test library
+if [ ! -d wordpress-tests-lib ]; then
+  mkdir wordpress-tests-lib
+  svn co --quiet https://develop.svn.wordpress.org/tags/$(wp core version --allow-root)/tests/phpunit/includes/ wordpress-tests-lib/includes
+  curl -o wordpress-tests-lib/wp-tests-config-sample.php https://develop.svn.wordpress.org/tags/$(wp core version --allow-root)/wp-tests-config-sample.php
+fi
+
+# unit tests config
+if [ ! -f wordpress-tests-lib/wp-tests-config.php ]; then
+  cp wordpress-tests-lib/wp-tests-config-sample.php wordpress-tests-lib/wp-tests-config.php
+  sed -i "s:dirname( __FILE__ ) . '/src/':'$(pwd)/':" wordpress-tests-lib/wp-tests-config.php
+  sed -i "s/youremptytestdbnamehere/$WORDPRESS_TESTS_DB_NAME/" wordpress-tests-lib/wp-tests-config.php
+  sed -i "s/yourusernamehere/$WORDPRESS_DB_USER/" wordpress-tests-lib/wp-tests-config.php
+  sed -i "s/yourpasswordhere/$WORDPRESS_DB_PASSWORD/" wordpress-tests-lib/wp-tests-config.php
+  sed -i "s|localhost|${WORDPRESS_DB_HOST}|" wordpress-tests-lib/wp-tests-config.php
+fi
+
+## install vendor scripts for unit tests
+#if [ ! -d vendor ]; then
+#  composer install --working-dir=$(wp plugin path --allow-root)/woocommerce-pos
+#fi
